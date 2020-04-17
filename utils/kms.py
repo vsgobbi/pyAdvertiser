b4c2cc51abfd8e2e05fd527827c6fbea591cda21
ongui <vitorsgobbi@hotmail.com>
2020-04-17 19:49:12 -0300

    updated API Kms module

--- utils/kms.py
+++ utils/kms.py
@@ -1,18 +1,16 @@
-import boto3
 from base64 import b64encode, b64decode
+from utils.configs import kmsClient, keyId
 
 
 class ApiKms(object):
 
-    kmsClient = boto3.client("kms", "us-east-1")
-    keyId = "the aws ksm generated key id"
-
     @classmethod
     def encrypt(cls, string):
-        cipher = cls.kmsClient.encrypt(KeyId=cls.keyId, Plaintext=str(string))
+        cipher = kmsClient.encrypt(KeyId=keyId, Plaintext=str(string))
         return b64encode(cipher["CiphertextBlob"])
 
     @classmethod
     def decrypt(cls, encodedCipher):
-        plainPassword = cls.kmsClient.decrypt(CiphertextBlob=bytes(b64decode(encodedCipher)))
+        plainPassword = kmsClient.decrypt(CiphertextBlob=bytes(b64decode(encodedCipher)))
         return plainPassword["Plaintext"].decode()
+
