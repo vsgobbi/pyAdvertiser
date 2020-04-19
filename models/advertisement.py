998e24b626f2135f2828b42b2b486f806d8b5e51
ongui <vitorsgobbi@hotmail.com>
2020-04-19 02:44:40 -0300

    fixes

--- models/advertisement.py
+++ models/advertisement.py
@@ -4,59 +4,109 @@ from pynamodb.models import Model
 from pynamodb.attributes import UTCDateTimeAttribute, UnicodeAttribute, UnicodeSetAttribute, NumberAttribute
 
 
-class Advertisement(object, Model):
+class Advertisement(Model):
 
     class Meta:
         table_name = "atnap_advertisement"
         region = region
 
+    title = UnicodeAttribute(null=False)
+    advertiserTaxId = UnicodeAttribute(null=False, hash_key=True)
+    category = UnicodeAttribute(null=False, range_key=True)
+    phoneNumber = UnicodeAttribute(null=False)
+    whatsAppApi = UnicodeAttribute(null=False)
+    picturesUrl = UnicodeAttribute(null=True)
+    socialMedia = UnicodeAttribute(null=True)
+    description = UnicodeAttribute(null=False)
+    tags = UnicodeSetAttribute(null=True)
+    price = NumberAttribute(null=False)
     created = UTCDateTimeAttribute(null=False, default=datetime.now())
     updated = UTCDateTimeAttribute(null=True)
-    deletedBy = UnicodeAttribute()
-    title = UnicodeAttribute(null=False, hash_key=True)
-    category = UnicodeAttribute(null=False, range_key=True)
-    picturesUrl = UnicodeAttribute(null=True)
-    description = UnicodeAttribute(null=False)
-    tags = UnicodeSetAttribute(null=False)
-    price = NumberAttribute(null=False)
+    deletedBy = UnicodeAttribute(null=True)
 
     @classmethod
-    def newItem(cls, fullName, companyName, taxId, email, phoneNumber):
-        advertiser = Advertisement(
-            fullName=fullName,
-            companyName=companyName,
-            taxId=taxId,
+    def newItem(cls, title, category, description, phoneNumber,
+                price, socialMedia, advertiserTaxId, whatsAppApi,
+                tags=None, picturesUrl=None):
+        advertisement = Advertisement(
+            title=title,
+            category=category,
+            description=description,
+            tags=tags,
+            price=price,
+            advertiserTaxId=advertiserTaxId,
             phoneNumber=phoneNumber,
-            email=email,
+            picturesUrl=picturesUrl,
+            whatsAppApi=whatsAppApi,
+            socialMedia=socialMedia,
         )
-        return advertiser.save()
+        return advertisement.save()
 
     @classmethod
     def deleteItem(cls, taxId, email):
-        advertiser = Advertisement(
+        advertisement = Advertisement(
             taxId=taxId,
             email=email
         )
-        return advertiser.delete()
+        return advertisement.delete()
 
     @classmethod
-    def detailedAdvertisement(cls, advertiser):
+    def updateItem(cls, **kwargs):
+        user = Advertisement.get(
+            kwargs.get("advertiserTaxId"),
+        )
+        user.refresh()
+        user.update(actions=[
+            Advertisement.phoneNumber.set(kwargs.get("phoneNumber")) or Advertisement.phoneNumber,
+            Advertisement.updated.set(datetime.now())
+        ])
+
+    @classmethod
+    def detailedAdvertisement(cls, advertisement):
         return {
-            "fullName": Advertisement.fullName,
-            "companyName": advertiser.companyName,
-            "taxId": advertiser.taxId,
-            "email": advertiser.email,
-            "phoneNumber": advertiser.phoneNumber,
-            "created": advertiser.created.strftime("%Y-%m-%d"),
+            "fullName": advertisement.fullName,
+            "companyName": advertisement.companyName,
+            "taxId": advertisement.taxId,
+            "email": advertisement.email,
+            "phoneNumber": advertisement.phoneNumber,
+            "created": advertisement.created.strftime("%Y-%m-%d"),
         }
 
     @classmethod
-    def json(cls, advertiser):
+    def createTable(cls):
+        if not Advertisement.exists():
+            Advertisement.create_table(
+                read_capacity_units=10,
+                write_capacity_units=10,
+                wait=True
+            )
+
+    @classmethod
+    def queryByTaxId(cls, taxId):
+        queryResult = []
+        for item in Advertisement.query(taxId):
+            queryResult.append(cls.json(item))
+
+        return queryResult
+
+    @classmethod
+    def dropTable(cls):
+        if Advertisement.exists():
+            Advertisement.delete_table()
+
+    @classmethod
+    def tableDefinitions(cls):
+        if Advertisement.exists():
+            return Advertisement.describe_table()
+
+    @classmethod
+    def json(cls, advertisement):
         return {
-            "fullName": Advertisement.fullName,
-            "companyName": advertiser.companyName,
-            "taxId": advertiser.taxId,
-            "email": advertiser.email,
-            "phoneNumber": advertiser.phoneNumber,
-            "created": advertiser.created.strftime("%Y-%m-%d"),
+            "title": advertisement.title,
+            "companyName": advertisement.description,
+            "taxId": advertisement.advertiserTaxId,
+            "whatsAppApi": advertisement.whatsAppApi,
+            "phoneNumber": advertisement.phoneNumber,
+            "price": advertisement.price,
+            "created": advertisement.created.strftime("%Y-%m-%d"),
         }
