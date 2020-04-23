71cf00d81dff648aa018c116726c9638967bf6f3
ongui <vitorsgobbi@hotmail.com>
2020-04-22 23:27:42 -0300

    updated configs to use kms key id as os environment

--- utils/configs.py
+++ utils/configs.py
@@ -1,8 +1,17 @@
 # AWS Configurations
 import boto3
+from uuid import uuid4
+import os
 
 # PynamoDB region:
 region = "us-east-1"
 
-kmsClient = boto3.client("kms", "us-east-1")
-keyId = "bff7ac3a-3ce6-40c9-825b-884d59bf84e2"
+# KMS configs:
+try:
+    kmsClient = boto3.client("kms", "us-east-1")
+    kmsKeyId = os.environ["KMS_KEY_ID"]
+    print(kmsKeyId)
+except Exception as error:
+    print("Set KMS key id as environment variable before using it!, {}".format(error))
+
+sessionSecretKey = "{0}-{1}".format(uuid4(), uuid4())
