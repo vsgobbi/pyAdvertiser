954cd1642eb875cf2636607c3343c5bd3399ad84
ongui <vitorsgobbi@hotmail.com>
2020-04-28 17:29:34 -0300

    refactor register get handler

--- handlers/register.py
+++ handlers/register.py
@@ -1,7 +1,7 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, session
 from models.user import User
 from utils.responses import ApiResponses
-from validators.authentication import authenticated
+from validators.authentication import authenticated, allowedUser, validateUser, validLoggedUser
 from validators.validators import ApiValidators
 
 register = Blueprint("register", __name__)
@@ -9,6 +9,7 @@ register = Blueprint("register", __name__)
 
 @register.route("/api/v1/register", methods=["GET"])
 @authenticated
+@allowedUser
 def get():
     queryStringtaxId = request.args.get("taxId")
     taxId, errors = ApiValidators.validateTaxId(queryStringtaxId)
@@ -16,30 +17,42 @@ def get():
     if len(errors) > 0:
         return ApiResponses.badRequestMessage(errors)
 
-    user = User.queryByTaxId(taxId)
-    if user:
-        return ApiResponses.successMessage(item=user)
+    if not validLoggedUser(taxId):
+        return ApiResponses.forbiddenMessage("Cadastro inválido!")
 
-    return ApiResponses.notFoundMessage("Usuário não encontrado")
+    try:
+        validLoggedUser(taxId)
+        user = validateUser(taxId)
+        return ApiResponses.successMessage(item=User.json(user))
+    except:
+        return ApiResponses.badRequestMessage("Usuário inválido!")
 
 
 @register.route("/api/v1/register", methods=["POST"])
-@authenticated
 def post():
     taxId = request.json.get("taxId")
     password = request.json.get("password")
     fullName = request.json.get("fullName")
     email = request.json.get("email")
     phoneNumber = request.json.get("phoneNumber")
+    permissions = request.json.get("permissions")
 
     taxId, errors = ApiValidators.validateTaxId(taxId)
     email, errors = ApiValidators.validateEmail(email)
     password, errors = ApiValidators.validatePassword(password)
+    permissions, errors = ApiValidators.validatePermissions(permissions)
     phoneNumber, errors = ApiValidators.validatePhone(phoneNumber)
     fullName, errors = ApiValidators.validateUsername(fullName)
 
     if len(errors) > 0:
         return ApiResponses.badRequestMessage(errors)
+
+    if session.get("loggedin"):
+        return ApiResponses.badRequestMessage(error="Usuário já logado")
+
+    user = User.queryUserByTaxId(taxId)
+    if user:
+        return ApiResponses.badRequestMessage(error="Usuário já existe, verifique sua conta...")
 
     try:
         User.newItem(
@@ -47,7 +60,8 @@ def post():
             password,
             taxId,
             email,
-            phoneNumber
+            phoneNumber,
+            permissions
         )
         return ApiResponses.successMessage(message="sucesso", item="Usuário '{}' foi registrado".format(fullName))
     except Exception as error:
@@ -55,5 +69,19 @@ def post():
 
 
 @register.route("/api/v1/register", methods=["DELETE"])
+@authenticated
+@allowedUser
 def delete():
-    return ApiResponses.successMessage(message="Rota deletar usuário ainda a ser implementada")
+
+    queryStringtaxId = request.args.get("taxId")
+    taxId, errors = ApiValidators.validateTaxId(queryStringtaxId)
+
+    if len(errors) > 0:
+        return ApiResponses.badRequestMessage(errors)
+
+    user = User.queryUserByTaxId(taxId)
+    if user:
+        user.delete()
+        return ApiResponses.successMessage(message="Usuário deletado com sucesso", item=User.json(user))
+
+    return ApiResponses.notFoundMessage("Usuário não encontrado")
